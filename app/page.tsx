9f1fa2ad1ac7972992703ad5298fbef40042c0f6
Alamin Shaikh <hello@octib.com>
2024-01-01 16:14:28 +0600

    Services section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedProperties from '@components/home/FeaturedProperties';
 import Hero from '@components/home/Hero';
+import Services from '@components/home/Services';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <FeaturedProperties />
+      <Services />
     </main>
   );
 }
---
+++ components/home/Services.tsx
@@ -0,0 +1,26 @@
+import styles from '@components/home/Services.module.css';
+import Link from 'next/link';
+import { RiVipDiamondLine } from 'react-icons/ri';
+
+export default function Services() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h2>
+          Full Experience <br />
+          Home Ownership
+        </h2>
+
+        <p>
+          At GemHaus, we believe that real estate ownership should be more than
+          just a transaction-it should be an enriching journey. Our mission is
+          to empower you to fully embrace the rewards of property ownership.
+        </p>
+
+        <Link className={styles.services_button} href='/'>
+          Our Services <RiVipDiamondLine />
+        </Link>
+      </div>
+    </section>
+  );
+}
