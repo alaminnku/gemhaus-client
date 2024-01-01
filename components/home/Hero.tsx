194c6c6cb1099924a53ab0adee2c99a5152dc307
Alamin Shaikh <hello@octib.com>
2024-01-01 13:54:43 +0600

    Home hero updated

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,10 +1,87 @@
+'use client';
+
 import Header from '@components/layout/Header';
 import styles from '@components/home/Hero.module.css';
+import { FormEvent, useState } from 'react';
+import { MdOutlineSearch } from 'react-icons/md';
 
 export default function Hero() {
+  const initialPropertyTypes = {
+    vacation: 'selected',
+    rent: 'first',
+    buy: 'second',
+  };
+
+  const [propertyTypes, setPropertyTypes] = useState(initialPropertyTypes);
+  const { vacation, rent, buy } = propertyTypes;
+
+  function changePropertyType(type: 'vacation' | 'rent' | 'buy') {
+    setPropertyTypes((prevState) => {
+      switch (type) {
+        case 'vacation':
+          return initialPropertyTypes;
+        case 'rent':
+          return { vacation: 'first', rent: 'selected', buy: 'second' };
+        case 'buy':
+          return { vacation: 'first', rent: 'second', buy: 'selected' };
+        default:
+          return prevState;
+      }
+    });
+  }
+
+  function handleSearch(e: FormEvent) {
+    e.preventDefault();
+
+    let selectedSearchType = '';
+    for (const prop in propertyTypes) {
+      if (propertyTypes[prop as keyof object] === 'selected') {
+        selectedSearchType = prop;
+      }
+    }
+
+    console.log(selectedSearchType);
+  }
+
   return (
     <section className={styles.container}>
       <Header />
+
+      <div className={styles.content}>
+        <h1>Find your gem.</h1>
+
+        <form className={styles.search} onSubmit={handleSearch}>
+          <div className={styles.search_types}>
+            <p
+              className={`${styles[vacation]} ${styles.vacation}`}
+              onClick={() => changePropertyType('vacation')}
+            >
+              Vacation
+            </p>
+            <p
+              className={`${styles[rent]} ${styles.rent}`}
+              onClick={() => changePropertyType('rent')}
+            >
+              Rent
+            </p>
+            <p
+              className={`${styles[buy]} ${styles.buy}`}
+              onClick={() => changePropertyType('buy')}
+            >
+              Buy
+            </p>
+          </div>
+
+          <div className={styles.search_field}>
+            <input
+              type='text'
+              placeholder='Enter address, neighborhood, city or ZIP code'
+            />
+
+            <MdOutlineSearch onClick={handleSearch} />
+          </div>
+        </form>
+      </div>
     </section>
   );
 }
