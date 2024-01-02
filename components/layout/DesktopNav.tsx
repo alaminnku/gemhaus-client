2cab9dd94b5b9402b07a9a1509cb3d89d52935b6
Alamin Shaikh <hello@octib.com>
2024-01-02 18:54:55 +0600

    Active style added to nav links

--- components/layout/DesktopNav.tsx
+++ components/layout/DesktopNav.tsx
@@ -1,19 +1,49 @@
+'use client';
+
 import styles from '@components/layout/DesktopNav.module.css';
 import Logo from './Logo';
 import Link from 'next/link';
 import { FaRegCircleUser } from 'react-icons/fa6';
+import { usePathname } from 'next/navigation';
 
 export default function DesktopNav() {
+  const pathname = usePathname();
+
   return (
     <nav className={styles.container}>
       <Logo />
 
       <div className={styles.navigation}>
-        <Link href='/vacation-rental'>Vacation Rental</Link>
-        <Link href='/long-term-rental'>Long Term Rental</Link>
-        <Link href='/buy-and-sell'>Buy & Sell</Link>
-        <Link href='/'>Blog</Link>
-        <Link href='/'>Contact</Link>
+        <Link
+          href='/vacation-rental'
+          className={pathname === '/vacation-rental' ? styles.active : ''}
+        >
+          Vacation Rental
+        </Link>
+        <Link
+          href='/long-term-rental'
+          className={pathname === '/long-term-rental' ? styles.active : ''}
+        >
+          Long Term Rental
+        </Link>
+        <Link
+          href='/buy-and-sell'
+          className={pathname === '/buy-and-sell' ? styles.active : ''}
+        >
+          Buy & Sell
+        </Link>
+        <Link
+          href='/blog'
+          className={pathname === '/blog' ? styles.active : ''}
+        >
+          Blog
+        </Link>
+        <Link
+          href='/contact'
+          className={pathname === '/contact' ? styles.active : ''}
+        >
+          Contact
+        </Link>
 
         <Link href='/' className={styles.sign_in}>
           Sign in <FaRegCircleUser />
