ff38cc7557a087c4fcaa55bbc14f38289516e76e
Alamin Shaikh <hello@octib.com>
2024-01-22 20:19:50 +0600

    Luxury gems and services sections deleted

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import FeaturedProperties from '@components/home/FeaturedProperties';
 import Hero from '@components/home/Hero';
 import Marketing from '@components/home/Marketing';
-import LuxuryGems from '@components/home/LuxuryGems';
-import Services from '@components/home/Services';
 import Blog from '@components/home/Blog';
 import FAQs from '@components/home/FAQs';
 import Newsletter from '@components/home/Newsletter';
@@ -13,8 +11,6 @@ export default function HomePage() {
       <Hero />
       <FeaturedProperties />
       <Marketing />
-      <LuxuryGems />
-      <Services />
       <Blog />
       <FAQs />
       <Newsletter />
