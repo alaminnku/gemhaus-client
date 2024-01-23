9c75dbbbc507193ab7db913447e1681c83f0f25c
Alamin Shaikh <hello@octib.com>
2024-01-23 13:43:52 +0600

    FAQ UI updated

--- components/home/FAQs.tsx
+++ components/home/FAQs.tsx
@@ -4,6 +4,8 @@ import styles from '@components/home/FAQs.module.css';
 import { useState } from 'react';
 import { faqs, initialFAQsState } from '@data/faqs';
 import { FAQsState } from 'types';
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
+import { MdOutlineKeyboardArrowUp } from 'react-icons/md';
 
 export default function FAQ() {
   const [faqsState, setFAQsState] = useState(initialFAQsState);
@@ -22,30 +24,35 @@ export default function FAQ() {
 
   return (
     <section className={styles.container}>
-      <span>FAQs</span>
       <h2>
         Your Questions, <br />
         Answered
       </h2>
 
-      {faqs.map(({ id, question, answer }) => (
-        <div className={styles.question_and_answer} key={id}>
-          <div className={styles.question}>
-            <p>{question}</p>
-            <span onClick={() => toggleFAQsState(id)}>
-              {faqsState[id].isOpen ? '-' : '+'}
-            </span>
-          </div>
+      <div className={styles.questions_and_answers}>
+        {faqs.map(({ id, question, answer }) => (
+          <div className={styles.question_and_answer} key={id}>
+            <div className={styles.question}>
+              <p>{question}</p>
+              {faqsState[id].isOpen ? (
+                <MdOutlineKeyboardArrowUp onClick={() => toggleFAQsState(id)} />
+              ) : (
+                <MdOutlineKeyboardArrowDown
+                  onClick={() => toggleFAQsState(id)}
+                />
+              )}
+            </div>
 
-          <p
-            className={`${styles.answer} ${
-              faqsState[id].isOpen && styles.show_answer
-            }`}
-          >
-            {answer}
-          </p>
-        </div>
-      ))}
+            <p
+              className={`${styles.answer} ${
+                faqsState[id].isOpen && styles.show_answer
+              }`}
+            >
+              {answer}
+            </p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
