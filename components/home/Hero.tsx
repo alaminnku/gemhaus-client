ddab99d2c5e1001f221e6f6a9b8877026f49cf2f
Alamin Shaikh <hello@octib.com>
2024-01-25 17:28:12 +0600

    Linked search types

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -10,92 +10,50 @@ import rentIcon from '@public/home/rent-icon.png';
 import hero from '@public/home/hero.jpg';
 import Image from 'next/image';
 import { inter } from '@lib/utils';
-
-type PropertyState = 'selected' | 'first' | 'second';
-
-type PropertyTypes = {
-  vacation: PropertyState;
-  rent: PropertyState;
-  buy: PropertyState;
-};
-
-const initialPropertyTypes: PropertyTypes = {
-  vacation: 'selected',
-  rent: 'first',
-  buy: 'second',
-};
+import Link from 'next/link';
 
 export default function Hero() {
-  const [propertyTypes, setPropertyTypes] = useState(initialPropertyTypes);
-  const { vacation, rent, buy } = propertyTypes;
-
-  function changePropertyType(type: keyof PropertyTypes) {
-    setPropertyTypes((prevState) => {
-      switch (type) {
-        case 'vacation':
-          return initialPropertyTypes;
-        case 'rent':
-          return { vacation: 'first', rent: 'selected', buy: 'second' };
-        case 'buy':
-          return { vacation: 'first', rent: 'second', buy: 'selected' };
-        default:
-          return prevState;
-      }
-    });
-  }
-
-  function handleSearch(formData: FormData) {
-    const search = formData.get('search');
-
-    let selectedPropertyType: keyof PropertyTypes = 'vacation';
-    for (let prop in propertyTypes) {
-      const property = prop as keyof PropertyTypes;
-      if (propertyTypes[property] === 'selected') {
-        selectedPropertyType = property;
-      }
-    }
-
-    console.log(selectedPropertyType, search);
-  }
+  const [search, setSearch] = useState('');
 
   return (
     <section className={styles.container}>
       <Header />
 
       <div className={styles.search_and_cta}>
-        <form className={styles.search} action={handleSearch}>
+        <div className={styles.search}>
           <div className={`${styles.search_types} ${inter.className}`}>
-            <div
-              className={`${styles[vacation]} ${styles.vacation}`}
-              onClick={() => changePropertyType('vacation')}
-            >
+            <div className={styles.vacation}>
               <Image src={vacationIcon} alt='Vacation icon' />
               <p>Vacation</p>
             </div>
 
-            <div
-              className={`${styles[rent]} ${styles.rent}`}
-              onClick={() => changePropertyType('rent')}
-            >
+            <Link href='/properties' className={styles.rent}>
               <Image src={rentIcon} alt='Rent icon' />
               <p>Rent</p>
-            </div>
+            </Link>
 
-            <div
-              className={`${styles[buy]} ${styles.buy}`}
-              onClick={() => changePropertyType('buy')}
-            >
+            <Link href='/buy-and-sell' className={styles.buy}>
               <Image src={buyIcon} alt='Vacation icon' />
               <p>Buy</p>
-            </div>
+            </Link>
           </div>
 
           <div className={`${styles.search_field} ${inter.className}`}>
-            <MdOutlineSearch onClick={handleSearch} />
-            <input type='text' name='search' placeholder='Find your gem' />
-            <input type='submit' className={styles.search_button} />
+            <MdOutlineSearch />
+
+            <input
+              type='text'
+              placeholder='Find your gem'
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <Link
+              className={styles.search_button}
+              href={`/properties/?search=${search}`}
+            >
+              Submit
+            </Link>
           </div>
-        </form>
+        </div>
 
         <div className={styles.cta}>
           <p className={styles.title}>What's next?</p>
