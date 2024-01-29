2bc053e1c626315c953076b9494eca96f3149088
Alamin Shaikh <hello@octib.com>
2024-01-29 13:24:07 +0600

    Expanding property image on click

--- components/vacationRental/PropertyImages.tsx
+++ components/vacationRental/PropertyImages.tsx
@@ -1,26 +1,83 @@
+'use client';
+
 import Image from 'next/image';
 import styles from './PropertyImages.module.css';
+import { useState } from 'react';
+import { IoIosCloseCircleOutline } from 'react-icons/io';
 
 type Props = {
   images: string[];
 };
 
 export default function PropertyImages({ images }: Props) {
+  const [expandedImage, setExpandedImage] = useState<string>('');
+
   return (
     <div className={styles.container}>
       <div className={styles.main_image}>
-        <Image src={images[0]} width={800} height={500} alt='Image' />
+        <Image
+          src={images[0]}
+          width={800}
+          height={500}
+          alt='Image'
+          onClick={() => setExpandedImage(images[0])}
+        />
       </div>
       <div className={styles.other_images}>
         {images.map((src, index) => (
           <>
-            <Image key={index} src={src} width={800} height={500} alt='Image' />
-            <Image key={index} src={src} width={800} height={500} alt='Image' />
-            <Image key={index} src={src} width={800} height={500} alt='Image' />
-            <Image key={index} src={src} width={800} height={500} alt='Image' />
+            <Image
+              key={index}
+              src={src}
+              width={800}
+              height={500}
+              alt='Image'
+              onClick={() => setExpandedImage(src)}
+            />
+            <Image
+              key={index}
+              src={src}
+              width={800}
+              height={500}
+              alt='Image'
+              onClick={() => setExpandedImage(src)}
+            />
+            <Image
+              key={index}
+              src={src}
+              width={800}
+              height={500}
+              alt='Image'
+              onClick={() => setExpandedImage(src)}
+            />
+            <Image
+              key={index}
+              src={src}
+              width={800}
+              height={500}
+              alt='Image'
+              onClick={() => setExpandedImage(src)}
+            />
           </>
         ))}
       </div>
+
+      {expandedImage && (
+        <div className={styles.expanded_image_container}>
+          <div className={styles.expanded_image}>
+            <Image
+              src={expandedImage}
+              alt='Expanded Image'
+              width={1600}
+              height={1000}
+            />
+            <IoIosCloseCircleOutline
+              className={styles.close_button}
+              onClick={() => setExpandedImage('')}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
