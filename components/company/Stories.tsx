78496e7805f30cade29c4560fe426bc1e4432672
Alamin Shaikh <hello@octib.com>
2024-01-30 12:25:32 +0600

    Stories updated

--- components/company/Stories.tsx
+++ components/company/Stories.tsx
@@ -6,57 +6,52 @@ import missionImage from '@public/company/mission.png';
 import whyUsImage from '@public/company/why-us.png';
 import communityImage from '@public/company/community-involvement.png';
 import { useState } from 'react';
+import { stories } from '@data/stories';
 
 export default function OurStory() {
-  const [selectedItem, setSelectedItem] = useState('');
+  const [selectedStory, setSelectedStory] = useState<string | null>(
+    stories[0].title
+  );
 
   return (
     <section className={styles.container}>
-      <div className={styles.story}>
-        <h2>Our Story</h2>
-        <div className={styles.content}>
-          <p>
-            GemHaus was born from a vision of innovation, excellence, and a
-            relentless pursuit of redefining the real estate experience. In a
-            rapidly evolving industry where the dynamics of real estate were
-            shifting, we recognized a need for experts who could navigate this
-            new age - a time where platforms like Airbnb were transforming the
-            way people interact with property.
-          </p>
-          <p>
-            Real estate isn't just about buying and selling properties; it's
-            about embracing change and leveraging opportunities. It's about
-            adapting to emerging trends, embracing the sharing economy, and
-            providing tailored solutions to property owners, investors, and
-            guests alike. At GemHaus, we've set out to create a dynamic and
-            forward-thinking real estate brokerage that doesn't just keep pace
-            with industry shifts but leads the way. We understand that success
-            in real estate requires expertise, adaptability, and a commitment to
-            meeting the unique needs of our clients.
-          </p>
-          <p>
-            Our journey is driven by the belief that the future of real estate
-            is an ever-evolving landscape where innovation meets tradition, and
-            where personalized service thrives alongside cutting-edge
-            technology. We're here to guide you through this exciting journey,
-            offering a new paradigm for real estate that redefines what's
-            possible.
-          </p>
-        </div>
-      </div>
+      {stories.map((story) => (
+        <div className={styles.story}>
+          <h2
+            onClick={() =>
+              setSelectedStory((prevState) =>
+                prevState === story.title ? null : story.title
+              )
+            }
+          >
+            {story.title}
+          </h2>
+          <div
+            className={`${styles.content} ${
+              selectedStory === story.title && styles.show_content
+            }`}
+          >
+            {story.content.map((el) => (
+              <p>{el}</p>
+            ))}
+          </div>
 
-      <div className={` ${styles.item} ${styles.mission}`}>
-        <h3>Mission and Values</h3>
-        <Image src={missionImage} alt='Mission image' />
-      </div>
-      <div className={styles.item}>
-        <h3>Why Choose Us</h3>
-        <Image src={whyUsImage} alt='Why choose us image' />
-      </div>
-      <div className={styles.item}>
-        <h3>Community Involvement</h3>
-        <Image src={communityImage} alt='Community involvement image' />
-      </div>
+          <div
+            className={`${styles.image} ${
+              selectedStory === story.title && styles.hide_image
+            } ${story.title === 'Mission and Values' && styles.mission_image}`}
+          >
+            {story.image && (
+              <Image
+                src={story.image}
+                width={400}
+                height={400}
+                alt='About GemHaus image'
+              />
+            )}
+          </div>
+        </div>
+      ))}
     </section>
   );
 }
--- data/stories.ts
+++ data/stories.ts
@@ -1,7 +1,7 @@
 export const stories = [
   {
     title: 'Our Story',
-    description: [
+    content: [
       'GemHaus was born from a vision of innovation, excellence, and a relentless pursuit of redefining the real estate experience. In a rapidly evolving industry where the dynamics of real estate were shifting, we recognized a need for experts who could navigate this new age - a time where platforms like Airbnb were transforming the way people interact with property',
       "Real estate isn't just about buying and selling properties; it's about embracing change and leveraging opportunities. It's about adapting to emerging trends, embracing the sharing economy, and providing tailored solutions to property owners, investors, and guests alike. At GemHaus, we've set out to create a dynamic and forward-thinking real estate brokerage that doesn't just keep pace with industry shifts but leads the way. We understand that success in real estate requires expertise, adaptability, and a commitment to meeting the unique needs of our clients.",
       "Our journey is driven by the belief that the future of real estate is an ever-evolving landscape where innovation meets tradition, and where personalized service thrives alongside cutting-edge technology. We're here to guide you through this exciting journey, offering a new paradigm for real estate that redefines what's possible.",
@@ -10,7 +10,7 @@ export const stories = [
   {
     title: 'Mission and Values',
     image: '/company/mission.png',
-    description: [
+    content: [
       'GemHaus was born from a vision of innovation, excellence, and a relentless pursuit of redefining the real estate experience. In a rapidly evolving industry where the dynamics of real estate were shifting, we recognized a need for experts who could navigate this new age - a time where platforms like Airbnb were transforming the way people interact with property',
       "Real estate isn't just about buying and selling properties; it's about embracing change and leveraging opportunities. It's about adapting to emerging trends, embracing the sharing economy, and providing tailored solutions to property owners, investors, and guests alike. At GemHaus, we've set out to create a dynamic and forward-thinking real estate brokerage that doesn't just keep pace with industry shifts but leads the way. We understand that success in real estate requires expertise, adaptability, and a commitment to meeting the unique needs of our clients.",
     ],
@@ -18,7 +18,7 @@ export const stories = [
   {
     title: 'Why Choose Us',
     image: '/company/why-us.png',
-    description: [
+    content: [
       'GemHaus was born from a vision of innovation, excellence, and a relentless pursuit of redefining the real estate experience. In a rapidly evolving industry where the dynamics of real estate were shifting, we recognized a need for experts who could navigate this new age - a time where platforms like Airbnb were transforming the way people interact with property',
       "Real estate isn't just about buying and selling properties; it's about embracing change and leveraging opportunities. It's about adapting to emerging trends, embracing the sharing economy, and providing tailored solutions to property owners, investors, and guests alike. At GemHaus, we've set out to create a dynamic and forward-thinking real estate brokerage that doesn't just keep pace with industry shifts but leads the way. We understand that success in real estate requires expertise, adaptability, and a commitment to meeting the unique needs of our clients.",
     ],
@@ -26,7 +26,7 @@ export const stories = [
   {
     title: 'Community Involvement',
     image: '/company/community-involvement.png',
-    description: [
+    content: [
       'GemHaus was born from a vision of innovation, excellence, and a relentless pursuit of redefining the real estate experience. In a rapidly evolving industry where the dynamics of real estate were shifting, we recognized a need for experts who could navigate this new age - a time where platforms like Airbnb were transforming the way people interact with property',
       "Real estate isn't just about buying and selling properties; it's about embracing change and leveraging opportunities. It's about adapting to emerging trends, embracing the sharing economy, and providing tailored solutions to property owners, investors, and guests alike. At GemHaus, we've set out to create a dynamic and forward-thinking real estate brokerage that doesn't just keep pace with industry shifts but leads the way. We understand that success in real estate requires expertise, adaptability, and a commitment to meeting the unique needs of our clients.",
     ],
