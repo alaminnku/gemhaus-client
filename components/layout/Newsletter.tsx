5b84d8924a00c2e7f865c25cc594a706f5d42a00
Alamin Shaikh <hello@octib.com>
2024-01-30 13:17:26 +0600

    Integrated subscriber api

--- components/layout/Newsletter.tsx
+++ components/layout/Newsletter.tsx
@@ -1,11 +1,21 @@
+'use client';
+
 import styles from '@components/layout/Newsletter.module.css';
 import SubmitButton from '@components/layout/SubmitButton';
+import { fetchGemhausData } from '@lib/utils';
 import newsletter from '@public/layout/newsletter.png';
 import Image from 'next/image';
 
 export default function Newsletter() {
   async function handleSubscribe(formData: FormData) {
-    'use server';
+    const { data, error } = await fetchGemhausData('/subscribers/add', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (error) return console.log(error);
+
+    console.log(data);
   }
   return (
     <section className={styles.container}>
