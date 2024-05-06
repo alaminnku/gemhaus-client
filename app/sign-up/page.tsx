e0a9c8f25e9d1e998af1405ad59e1c3f611d3b78
Alamin Shaikh <hello@octib.com>
2024-05-06 13:04:57 +0600

    Updated component names and uninstalled mongodb

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -3,7 +3,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@lib/auth';
 import { redirect } from 'next/navigation';
 
-export default async function page() {
+export default async function SignUpPage() {
   const session = await getServerSession(authOptions);
   if (session && session.user) redirect('/');
 
--- components/signIn/SignIn.tsx
+++ components/signIn/SignIn.tsx
@@ -2,7 +2,7 @@ import Hero from './Hero';
 import Form from './Form';
 import styles from './SignIn.module.css';
 
-export default function SignUp() {
+export default function SignIn() {
   return (
     <section className={styles.container}>
       <Hero />
